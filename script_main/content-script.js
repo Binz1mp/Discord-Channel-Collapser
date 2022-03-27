aa9409b4747d44d9c869769415a7ad4884e785f0
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-03-27 21:30:24 +0900

    code minified

--- script_main/content-script.js
+++ script_main/content-script.js
@@ -15,8 +15,6 @@ function mainInjection() {
     } else {
       console.log("Hello!");
       }
-    
-
       for (i = 0; i < collapseButton.length; i++) {
         collapseButton[i].addEventListener("click", function () {
           if (window.location.href.indexOf('discord.com/channels/') > -1 && targetStuff.style.display === "flex") {
@@ -42,62 +40,6 @@ function mainInjection() {
     } else {
         console.log("Now you are in the lobby. which means there's no button to modify now.");
       }
-
-
-
-
-
-
   }
 };
 setTimeout(mainInjection,3500);
-
-
-
-
-
-
-
-//     if (document.getElementsByClassName('collapseParent').length >= 2) {
-//       document.querySelector('.collapseParent').remove('.related-collapse');
-//       console.log("killed duplicated button");
-//     } else {
-//       console.log("seems like normal?");
-//       }
-//     for (i = 0; i < collapseButton.length; i++) {
-//         collapseButton[i].addEventListener("click", function () {
-//           // var fulltarget = document.querySelector("#related").style.display;
-//           if (window.location.href.indexOf('youtube.com/watch') > -1 && targetStuff.style.display === "") {
-//               targetStuff.style.display = "none";
-//               // targetStuff2.style.display = "none";
-//               // START_IN_THEATER_MODE = "true";
-//               localStorage.setItem("related_display", 'display_none');
-//               document.querySelector(".related-collapse").textContent = 'Open Related Videos';
-//               console.log("버튼클릭 로컬스토리지 = " + localStorage.getItem('related_display'));
-//           } else {
-//               targetStuff.style.display = "";
-//               // targetStuff2.style.display = "";
-//               // START_IN_THEATER_MODE = "false";
-//               localStorage.setItem("related_display", 'display_normal');
-//               console.log("버튼클릭 로컬스토리지 = " + localStorage.getItem('related_display'));
-//               document.querySelector(".related-collapse").textContent = 'Close Related Videos';
-//           }
-//         });
-//     };
-//     if (window.location.href.indexOf('youtube.com/watch') > -1 &&targetStuff && collapseButton && localStorage.getItem('related_display') === 'display_none') {
-//       targetStuff.style.display = "none";
-//       // targetStuff2.style.display = "none";
-//       // START_IN_THEATER_MODE = "true";
-//       document.querySelector(".related-collapse").textContent = 'Open Related Videos';
-//       console.log("autoTheathreModeOn");
-//       }
-//     else if (window.location.href.indexOf('youtube.com/watch') > -1 &&targetStuff && collapseButton && localStorage.getItem('related_display') === 'display_normal') {
-//       targetStuff.style.display = "";
-//       // targetStuff2.style.display = "";
-//       // START_IN_THEATER_MODE = "false";
-//       document.querySelector(".related-collapse").textContent = 'Close Related Videos';
-//       console.log("autoTheathreModeOff");
-//     } else {
-//         console.log("Now you are in the lobby. which means there's no button to modify now.");
-//       }
-// });
