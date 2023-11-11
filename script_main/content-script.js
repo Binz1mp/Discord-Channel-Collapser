0a2c6dec42270ed7eeae83b03cfb266ae832d69f
Binz1mp <binieon@naver.com>
2023-11-11 11:23:13 +0900

    롤백하고 다시 올림 - 디스코드 니트로 테마 변경하면 요소 바뀌어서 일단 내가 쓰는 테마만 되게 임시로 푸시

--- script_main/content-script.js
+++ script_main/content-script.js
@@ -1,52 +1,47 @@
-// function loadingremover() {
-//   if (window.location.href.indexOf('discord.com/channels/') > -1) {
-//     // const loadingStuff = document.querySelector(".container-2RRFHK.fixClipping-3GOd_d");
-//     // console.log("before_____loadingremover");
-//     // loadingStuff.style.display = "none";
-//     // console.log("after_____loadingremover");
-//   }
-// }
-// setTimeout(loadingremover,500);
-
+let consoleStyle = "text-shadow: 1px 1px 2px black, 0 0 1em blue, 0 0 0.2em blue; font-size: 40px; background-color: white; color: #181818"
 function mainInjection() {
   if (window.location.href.indexOf('discord.com/channels/') > -1) {
+    console.log("%cDiscord Channel Collapser is WORKING", consoleStyle);
     const injectElement = document.createElement('div');
     injectElement.classList.add('collapseParent');
     let collapseButton = document.getElementsByClassName("popcatChan-collapse");
-    const targetStuff = document.querySelector(".sidebar-1tnWFu");
+    const targetStuff = document.querySelector(".sidebar_ded4b5"); // 사이드바
+    // let messageItems = document.getElementsByClassName('cozy-VmLDNB wrapper-30-Nkg'); // 유저들의 메시지
+    // let avatarImgs = document.getElementsByClassName('avatar-2e8lTP'); // 유저들의 프로필 사진
     injectElement.innerHTML = '<div class="popcatChan-collapse" alt=""></div>';
-    const insertbeforestuff = document.querySelector(".tutorialContainer-1pL9QS");
-    if (document.querySelector(".scroller-3X7KbA.none-2-_0dP.scrollerBase-_bVAAt")) {
+    const insertbeforestuff = document.querySelector(".tutorialContainer__890ea"); // 팝캣 버튼을 배치할 위치
+    if (document.querySelector(".scroller__3d071.none__51a8f.scrollerBase_dc3aa9")) {
       console.log("before_____operated");
-      document.querySelector(".scroller-3X7KbA.none-2-_0dP.scrollerBase-_bVAAt").insertBefore(injectElement, insertbeforestuff);
+      document.querySelector(".scroller__3d071.none__51a8f.scrollerBase_dc3aa9").insertBefore(injectElement, insertbeforestuff);
       console.log("after_____operated");
-    } else {
-      console.log("Hello!");
-      }
-      for (i = 0; i < collapseButton.length; i++) {
-        collapseButton[i].addEventListener("click", function () {
-          if (window.location.href.indexOf('discord.com/channels/') > -1 && targetStuff.style.display === "flex") {
-            targetStuff.style.display = "none";
-            localStorage.setItem("sidebar-1tnWFu", 'display_none');
-            // document.querySelector(".popcatChan-collapse").textContent = 'Open';
-          } else {
-              targetStuff.style.display = "flex";
-                localStorage.setItem("sidebar-1tnWFu", 'display_flex');
-                // document.querySelector(".popcatChan-collapse").textContent = 'Close';
-            }
+    }
+    for (i = 0; i < collapseButton.length; i++) {
+      collapseButton[i].addEventListener("click", function () {
+        if (window.location.href.indexOf('discord.com/channels/') > -1 && targetStuff.style.display === "flex") {
+          targetStuff.style.display = "none";
+          localStorage.setItem("sidebar_ded4b5", 'display_none');
+          // for (let i = 0; i < avatarImgs.length; i++) { avatarImgs[i].style.display = "none"; }
+          // for (let i = 0; i < messageItems.length; i++) { messageItems[i].style.paddingLeft = "15px"; }
+        } else {
+            targetStuff.style.display = "flex";
+            localStorage.setItem("sidebar_ded4b5", 'display_flex');
+            // for (let i = 0; i < avatarImgs.length; i++) { avatarImgs[i].style.display = "initial"; }
+            // for (let i = 0; i < messageItems.length; i++) { messageItems[i].style.paddingLeft = "72px"; }
           }
-        )
-      }
-    if (window.location.href.indexOf('discord.com/channels/') > -1 &&targetStuff && collapseButton && localStorage.getItem('sidebar-1tnWFu') === 'display_none') {
+        }
+      )
+    }
+    if (window.location.href.indexOf('discord.com/channels/') > -1 &&targetStuff && collapseButton && localStorage.getItem('sidebar_ded4b5') === 'display_none') {
       targetStuff.style.display = "none";
-      // document.querySelector(".popcatChan-collapse").textContent = 'Open';
-      }
-    else if (window.location.href.indexOf('discord.com/channels/') > -1 &&targetStuff && collapseButton && localStorage.getItem('sidebar-1tnWFu') === 'display_flex') {
+      // for (let i = 0; i < avatarImgs.length; i++) { avatarImgs[i].style.display = "none";  }
+      // for (let i = 0; i < messageItems.length; i++) { messageItems[i].style.paddingLeft = "15px"; }
+    } else if (window.location.href.indexOf('discord.com/channels/') > -1 &&targetStuff && collapseButton && localStorage.getItem('sidebar_ded4b5') === 'display_flex') {
       targetStuff.style.display = "flex";
-      // document.querySelector(".popcatChan-collapse").textContent = 'Close';
+      // for (let i = 0; i < avatarImgs.length; i++) { avatarImgs[i].style.display = "initial"; }
+      // for (let i = 0; i < messageItems.length; i++) { messageItems[i].style.paddingLeft = "72px"; }
     } else {
         console.log("Now you are in the lobby. which means there's no button to modify now.");
       }
   }
 };
-setTimeout(mainInjection,5000);
+setTimeout(mainInjection, 5000);
